6a6e93230f49b9b19b4bdbc862a0cf04a5547028
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-02-14 22:20:00 -0500

    created poll model

--- models/poll.js
+++ models/poll.js
@@ -1,3 +1,36 @@
 const sequelize = require('./config/connection');
 const { Model, DataTypes } = require('sequelize');
 
+class Poll extends Model {}
+
+Poll.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: false,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATEONLY
+        },
+        expiration_date: {
+            type: DataTypes.DATEONLY,
+        },
+    },
+)
