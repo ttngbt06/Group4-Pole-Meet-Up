2cebf540b5bc63655a9bb668d74ed43e3e80171c
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-02-15 19:58:31 -0500

    finished child controller index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,16 @@
 const router = require('express').Router();
+const categoriesRoutes = require('./categoriesRoutes');
+const optionsRoutes = require('./optionsRoutes');
+const pollCategoriesRoutes = require('./poll-categoriesRoutes');
+const pollsRoutes = require('./pollRoutes');
+const usersRoutes = require('./usersRoutes');
+const votesRoutes = require('./votesRoutes');
+
+router.use('/categories', categoriesRoutes);
+router.use('/options', optionsRoutes);
+router.use('/poll-categories', pollCategoriesRoutes);
+router.use('/polls', pollsRoutes);
+router.use('/users', usersRoutes);
+router.use('/votes', votesRoutes);
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./home-routes');
+const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 
