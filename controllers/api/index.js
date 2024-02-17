c6c6b253fbe299cfcaaf678c5127497550133a35
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-02-17 14:37:11 -0500

    re-filled index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,11 +6,6 @@ const pollsRoutes = require('./pollRoutes');
 const usersRoutes = require('./usersRoutes');
 const votesRoutes = require('./votesRoutes');
 
-router.use('/categories', categoriesRoutes);
-router.use('/options', optionsRoutes);
-router.use('/poll-categories', pollCategoriesRoutes);
-router.use('/polls', pollsRoutes);
-router.use('/users', usersRoutes);
-router.use('/votes', votesRoutes);
+
 
 module.exports = router;
